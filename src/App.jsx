6681c9c8de8e87b27fccce68303e8de1c3de0823
9ightshade <aniekemeusanga@gmail.com>
2023-12-10 00:04:13 +0100

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 function App() {
-  return <h1 className="text-3xl font-bold underline bg-black ">Hello world!</h1>;
+  return <h1 className="text-3xl font-bold underline bg-black text-[blue] ">Hello world!</h1>;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./components/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//     theme: {
+//         extend: {
+//             colors:{
+//                 primary:'#ddd',
+//             },
+//             screens:{
+//                 xmd:"500px",
+//                 xxmd:"550px"
+//             }
+//         },
+//     },
+//     plugins: [],
+// };
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
+//   // ...
+// };
