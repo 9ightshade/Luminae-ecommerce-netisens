19a02864299451b4a9e6de20d2972d3b18290292
9ightshade <aniekemeusanga@gmail.com>
2023-12-09 23:37:27 +0100

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
-import "./App.css";
 
 function App() {
-  return (
-    <div>
-      <h1>Luminae Ecommerce</h1>
-    </div>
-  );
+  return <h1 className="text-3xl font-bold underline bg-black ">Hello world!</h1>;
 }
 
 export default App;
