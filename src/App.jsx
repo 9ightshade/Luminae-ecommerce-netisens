5d5f04a3da967e7c798e6b07dfaba0de4cd34aeb
9ightshade <aniekemeusanga@gmail.com>
2023-12-10 00:28:27 +0100

    component folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 function App() {
   return (
-    <div className="h-[200px] bg-black">
-      <p className="text-[blue]" >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
-        provident corporis magnam incidunt at tempora. Eius numquam laborum
-        assumenda fugiat.
-      </p>
+    <div>
+      <h1 className="text-center  text-[3rem] " >
+        Luminae Ecommerce
+    </h1>
     </div>
   );
 }
