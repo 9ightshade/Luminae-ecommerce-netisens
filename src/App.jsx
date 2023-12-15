07592e22a2df270f9560b0093980f0eda5c3ac00
9ightshade <aniekemeusanga@gmail.com>
2023-12-15 09:03:29 +0100

    clear app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,8 @@
-// import { Logo } from "./components/shared/logo";
-// import { Concerns } from "./components/aboutUs/concernsForm";
-// import { Intro, TeamPhoto } from "./components/aboutUs/intro";
 
-import { SignUp } from "./components/signup/signUp";
-import { SignIn } from "./components/signin/signIn";
-import { Changepassword } from "./components/signup/forgotten";
-
-// import { Services } from "./components/aboutUs/services";
 function App() {
   return (
     <div>
-      {/* <Intro />
-      <TeamPhoto />
-      <Services />
-      <Concerns /> */}
-      <SignIn />       <SignUp />
-      <Changepassword/>
+    
 
     </div>
   );
