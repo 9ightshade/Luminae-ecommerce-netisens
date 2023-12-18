9ce68fff1689bac3d30d91c035fac6a68152c887
9ightshade <aniekemeusanga@gmail.com>
2023-12-18 13:03:56 +0100

    customer info form

--- src/components/cardCheckout/CustomerInfo.jsx
+++ src/components/cardCheckout/CustomerInfo.jsx
@@ -1,14 +1,60 @@
 export const CustomerInfo = () => {
-    
-    return (
-        <div>
-            
+  return (
+    <form className="p-[2em]">
+      <h1 className="text-[#262626] text-[1.25rem] font-[700] mb-[1em] ">
+        Customer Information
+      </h1>
+      <label htmlFor="email" className="text-[#555] text-[.8rem] font-[600]">
+        E-mail
+      </label>
+      <input
+        type="email"
+        name="email"
+        placeholder="Enter your email"
+        id="customer_email"
+        className="w-[100%] block rounded-[4px] border-[#D9D9D9] p-[.8em] border-[1px] bg-[#FFF]"
+      />
 
+      <section className="flex gap-[1.5em] mt-[1em] ">
+        <label
+          htmlFor="firstname"
+          className="text-[#555] text-[.8rem] font-[600]"
+        >
+          First Name{" "}
+          <input
+            type="text"
+            name=""
+            id=""
+            className="rounded-[4px] border-[#D9D9D9] w-[100%] border-[1px] bg-[#FFF]"
+          />
+        </label>
 
-            
-    </div>
+        <label
+          htmlFor="lastname"
+          className="text-[#555] text-[.8rem] font-[600]"
+        >
+          Last Name{" "}
+          <input
+            type="text"
+            name=""
+            id=""
+            className="rounded-[4px] border-[#D9D9D9] w-[100%] border-[1px] bg-[#FFF]"
+          />
+        </label>
+      </section>
 
+      <h3 className="text-[#262626] text-[1rem] font-[700]">
+        Shipping Address
+      </h3>
 
-)
-
-}
+      <label htmlFor="country">
+        <select name="country" id="country" className="block w-[100%]" >
+          <option value="Nigeria">Nigeria</option>
+          <option value="Canada">Canada</option>
+          <option value="Iceland">Iceland</option>
+          <option value="Burmingham">Burmingham</option>
+        </select>
+      </label>
+    </form>
+  );
+};
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,4 +1,5 @@
-import { CartContent } from "../components/cardCheckout/CartContent";
+// import { CartContent } from "../components/cardCheckout/CartContent";
+import { CustomerInfo } from "../components/cardCheckout/CustomerInfo";
 import { NavCheckout } from "../components/cardCheckout/Navcheckout";
 import { OrderSummary } from "../components/cardCheckout/OrderSummary";
 import { Recommendations } from "../components/cardCheckout/Recommendations";
@@ -11,7 +12,7 @@ export const Checkout = () => {
       <Topnav/>
   <NavCheckout/>
       <div className="md:flex md:px-[2em] py-[2em] " >
-        <CartContent /> <OrderSummary />
+        <CustomerInfo/> <OrderSummary />
       </div>
       <Recommendations />
     </div>
