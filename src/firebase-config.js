5f0eb91f24e3f02f28a684c791c4ef2de323383b
9ightshade <aniekemeusanga@gmail.com>
2023-12-29 12:46:46 +0100

    firestore installation

---
+++ src/firebase-config.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD3B9uHbza-8H8CzVeGWfQfEKInxLuucpA",
+    authDomain: "luminae-ecommerce.firebaseapp.com",
+    projectId: "luminae-ecommerce",
+    storageBucket: "luminae-ecommerce.appspot.com",
+    messagingSenderId: "816782956062",
+    appId: "1:816782956062:web:5bf0c83b4ae500a4a9800f"
+  };
+
+  const app = initializeApp()
